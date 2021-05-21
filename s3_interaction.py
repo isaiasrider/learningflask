eca09b0a513dadc038107ef922971febb6730e54
Isaias <isaiasdelima1@gmail.com>
2021-05-20 22:31:54 -0300

    crud deployed

---
+++ s3_interaction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import boto3
+
+BUCKET_NAME = 'guerysbucket'
+
+s3_resources = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+def list_buckets():
+ for item in s3_resources.buckets.all():
+     print(item.name)
+
+def upload(file):
+
+    s3_client.upload_file(file, BUCKET_NAME, file)
+
+def delete_object(file_name,bucket):
+    s3_resources.Object(bucket,file_name).delete()
+
